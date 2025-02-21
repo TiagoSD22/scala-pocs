ae5fdf5662a2d150c826a7d32a1e05761fb549f1
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-02-21 17:16:41 -0300

    adding some logs

--- guitar_factory/src/main/scala/controllers/GuitarFactory.scala
+++ guitar_factory/src/main/scala/controllers/GuitarFactory.scala
@@ -8,22 +8,33 @@ import org.http4s.HttpRoutes
 import org.http4s.dsl.io.*
 import org.http4s.circe.*
 import utils.Inventory
+import org.log4s.getLogger
 
 object GuitarFactory {
+  private[this] val logger = getLogger
+
   import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
 
   def routes(inventory: Inventory): HttpRoutes[IO] = HttpRoutes.of[IO] {
     case req @ POST -> Root / "create-guitar" =>
       for {
         guitar <- req.as[Guitar]
+        _ = logger.info(s"Received request to create guitar: $$guitar")
         _ <- inventory.addGuitar(guitar)
         resp <- Ok(guitar.asJson)
-      } yield resp
+      } yield {
+        logger.info(s"Guitar created: $$guitar")
+        resp
+      }
 
     case GET -> Root / "inventory" =>
       for {
         guitars <- inventory.getInventory
+        _ = logger.info("Received request to get inventory")
         resp <- Ok(guitars.asJson)
-      } yield resp
+      } yield {
+        logger.info(s"Inventory retrieved: $$guitars")
+        resp
+      }
   }
 }
