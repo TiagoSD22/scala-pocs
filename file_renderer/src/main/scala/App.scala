fdbfcfdc2a28494ccecec37b62c20f6b0ed25fff
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-02-27 16:23:39 -0300

    Wrapping the get_renderer instruction with the Try from scala utils to not throw the exception

--- file_renderer/src/main/scala/App.scala
+++ file_renderer/src/main/scala/App.scala
@@ -1,11 +1,16 @@
 import renderer.TemplateRendererFactory
+import scala.util.{Try, Success, Failure}
 
 object App extends App {
   val template = "Hello, World!"
-  val format = "html" // Change to "pdf" or "csv" as needed
+  val format = "html"
 
-  val renderer = TemplateRendererFactory.getRenderer(format)
-  val renderedOutput = renderer.render(template)
-
-  println(renderedOutput)
+  val renderer: Unit = Try(TemplateRendererFactory.getRenderer(format)) match {
+    case Success(r) =>
+      val renderedOutput = r.render(template)
+      println(renderedOutput)
+    case Failure(e) =>
+      println(s"Error: ${e.getMessage}")
+      sys.exit(1)
+  }
 }
