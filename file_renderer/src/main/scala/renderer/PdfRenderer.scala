9bb197c34e4e8e580e9647f33fcc4451330c506a
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-02-27 16:22:45 -0300

    removed unnecessary comment

--- file_renderer/src/main/scala/renderer/PdfRenderer.scala
+++ file_renderer/src/main/scala/renderer/PdfRenderer.scala
@@ -2,7 +2,6 @@ package renderer
 
 class PdfRenderer extends TemplateRenderer {
   override def render(template: String): String = {
-    // Implement PDF rendering logic here
     s"PDF: $template"
   }
 }
