32ab1f74c530dff3922c3e0df9bb0f561b1226de
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-02-27 16:22:55 -0300

    removed unnecessary comment

--- file_renderer/src/main/scala/renderer/CsvRenderer.scala
+++ file_renderer/src/main/scala/renderer/CsvRenderer.scala
@@ -2,7 +2,6 @@ package renderer
 
 class CsvRenderer extends TemplateRenderer {
   override def render(template: String): String = {
-    // Implement CSV rendering logic here
     template.split("\n").map(_.split(",").mkString(",")).mkString("\n")
   }
 }
