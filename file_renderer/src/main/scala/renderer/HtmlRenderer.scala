9a00d02965b85fc016473d22a98448e545625187
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-02-27 16:22:50 -0300

    removed unnecessary comment

--- file_renderer/src/main/scala/renderer/HtmlRenderer.scala
+++ file_renderer/src/main/scala/renderer/HtmlRenderer.scala
@@ -2,7 +2,6 @@ package renderer
 
 class HtmlRenderer extends TemplateRenderer {
   override def render(template: String): String = {
-    // Implement HTML rendering logic here
     s"<html><body>$template</body></html>"
   }
 }
