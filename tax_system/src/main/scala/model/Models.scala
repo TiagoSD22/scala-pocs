4c7c00e67cddcf35c358ffa8073e0501fc45bcbe
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-03-10 12:41:34 -0300

    add models for product, state and taxRate

---
+++ tax_system/src/main/scala/model/Models.scala
@@ -0,0 +1,6 @@
+package model
+
+case class Product(id: String, name: String, price: BigDecimal)
+case class State(name: String)
+case class TaxRate(state: State, year: Int, rate: BigDecimal)
+case class TaxCalculation(product: Product, state: State, year: Int, taxAmount: BigDecimal)
