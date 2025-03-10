a3359ac441bcb4a38a055457a255f171102b1a0b
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-03-10 12:41:21 -0300

    add tax calculator

---
+++ tax_system/src/main/scala/service/TaxCalculator.scala
@@ -0,0 +1,13 @@
+package service
+
+import model.{Product, State, TaxCalculation}
+import zio.Task
+
+class TaxCalculator(taxService: TaxService) {
+  def calculateTax(product: Product, state: State, year: Int): Task[TaxCalculation] = {
+    for {
+      taxRate <- taxService.getTaxRate(state, year)
+      taxAmount = product.price * taxRate.rate
+    } yield TaxCalculation(product, state, year, taxAmount)
+  }
+}
