6e69712fc9adb618b45f680c1b10f553c30fc93e
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-03-11 17:06:31 -0300

    fix app to run without zio task

--- tax_system/src/main/scala/App.scala
+++ tax_system/src/main/scala/App.scala
@@ -1,25 +1,27 @@
 import model.{Product, State, TaxRate}
 import service.{TaxCalculator, TaxService}
-import zio.{ExitCode, Task, ULayer, ZIO, ZLayer}
+import zio.{ExitCode, ULayer, ZIO, ZLayer}
 
 object App extends zio.ZIOAppDefault {
+
+  val product = Product("1", "Laptop", 1000.00)
+  val state = State("California")
+  val year = 2023
+
   val taxServiceLayer: ULayer[TaxService] = ZLayer.succeed(new TaxService {
-    override def getTaxRate(state: State, year: Int): ZIO[Any, Nothing, TaxRate] = {
-      ZIO.succeed(TaxRate(state, year, BigDecimal(0.07)))
-    }
+    override def getTaxRate(state: State, year: Int): ZIO[Any, Nothing, TaxRate] =
+      ZIO.succeed(TaxRate(state, year, 0.075))
   })
 
-  val program: ZIO[TaxService, Throwable, Unit] = for {
-    product <- ZIO.succeed(Product("1", "Laptop", BigDecimal(1000)))
-    state <- ZIO.succeed(State("California"))
-    year <- ZIO.succeed(2023)
-    taxCalculator = new TaxCalculator(new TaxService {
-      override def getTaxRate(state: State, year: Int): Task[TaxRate] = TaxService.getTaxRate(state, year)
-    })
-    taxCalculation <- taxCalculator.calculateTax(product, state, year)
-    _ <- ZIO.succeed(println(s"Tax Calculation: $taxCalculation"))
-  } yield ()
+  val taxCalculatorLayer: ULayer[TaxCalculator] = taxServiceLayer >>> ZLayer.fromFunction(new TaxCalculator(_))
 
-  override def run: ZIO[zio.ZEnv, Nothing, ExitCode] =
-    program.provideLayer(taxServiceLayer).exitCode
+  override def run: ZIO[Any, Nothing, ExitCode] = {
+    val taxCalculation = for {
+      taxCalculator <- ZIO.service[TaxCalculator]
+      calculation <- taxCalculator.calculateTax(product, state, year)
+      _ <- ZIO.succeed(println(s"Tax Calculation: $calculation"))
+    } yield ()
+
+    taxCalculation.provideLayer(taxCalculatorLayer).exitCode
+  }
 }
