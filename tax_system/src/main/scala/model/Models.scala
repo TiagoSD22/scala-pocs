290b0c2d73bbf90ec3355451c2c2f8a0c38eeda2
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-03-12 17:43:36 -0300

    adding the tax rate to the taxCalculation object

--- tax_system/src/main/scala/model/Models.scala
+++ tax_system/src/main/scala/model/Models.scala
@@ -3,4 +3,4 @@ package model
 case class Product(id: String, name: String, price: BigDecimal)
 case class State(name: String)
 case class TaxRate(state: State, year: Int, rate: BigDecimal)
-case class TaxCalculation(product: Product, state: State, year: Int, taxAmount: BigDecimal)
+case class TaxCalculation(product: Product, state: State, year: Int, taxRate: BigDecimal, taxAmount: BigDecimal)
