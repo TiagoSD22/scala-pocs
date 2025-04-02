06cfff42c22796465e8cd59b2323e8aa43a5765a
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-04-01 22:58:04 -0300

    added port and address values for client and server to make sure they use the same values

--- redis_clone/src/main/scala/chapter3/App.scala
+++ redis_clone/src/main/scala/chapter3/App.scala
@@ -1,14 +1,16 @@
 package chapter3
 
-import scala.concurrent.{Future, ExecutionContext}
-import scala.util.{Failure, Success}
+import scala.concurrent.{ExecutionContext, Future}
 
 object App {
   def main(args: Array[String]): Unit = {
     implicit val ec: ExecutionContext = ExecutionContext.global
 
     val protocol = "tcp" // or udp
-    val server = new Server(protocol)
+    val port = 8234
+    val address = "127.0.0.1"
+
+    val server = new Server(protocol, port, address)
 
     Future {
       server.start()
@@ -16,7 +18,7 @@ object App {
 
     Thread.sleep(1000) // Wait for the server to start
 
-    val client = new Client(protocol)
+    val client = new Client(protocol, port, address)
     client.sendMessage("hello!")
   }
 }
