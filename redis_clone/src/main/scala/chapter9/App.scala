1870d75e5400dccec4428711110ba67dad31a7f3
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-04-10 22:56:07 -0300

    added main class to run client

---
+++ redis_clone/src/main/scala/chapter9/App.scala
@@ -0,0 +1,20 @@
+package chapter9
+
+object App {
+  def main(args: Array[String]): Unit = {
+    val client = new Client("127.0.0.1", 1234)
+    try {
+      val cmd = args.toList
+      if (client.sendRequest(cmd) != 0) {
+        println("Error sending request")
+        return
+      }
+      if (client.readResponse() != 0) {
+        println("Error reading response")
+        return
+      }
+    } finally {
+      client.close()
+    }
+  }
+}
