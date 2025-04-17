bf343b9dcb679494b15a2f01fea43f5827780420
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-04-16 22:15:54 -0300

    added method to run server

--- redis_clone/src/main/scala/chapter11/Server.scala
+++ redis_clone/src/main/scala/chapter11/Server.scala
@@ -55,3 +55,38 @@ def handleRequest(cmd: List[String], conn: Conn): Unit = {
   }
 }
 
+// Main server loop
+def startServer(port: Int): Unit = {
+  val serverSocket = new ServerSocket(port)
+  Utils.log(s"Server started on port $port")
+
+  while (true) {
+    val clientSocket = serverSocket.accept()
+    val conn = new Conn(clientSocket)
+
+    // Handle client connection
+    Try {
+      val input = clientSocket.getInputStream
+      val output = clientSocket.getOutputStream
+
+      // Read data
+      val buffer = new Array[Byte](1024)
+      val bytesRead = input.read(buffer)
+      if (bytesRead > 0) {
+        conn.incoming.append(buffer.take(bytesRead))
+        parseRequest(conn.incoming.toArray) match {
+          case Success(cmd) =>
+            handleRequest(cmd, conn)
+            output.write(conn.outgoing.toArray)
+          case Failure(ex) =>
+            Utils.logError("Failed to parse request", ex)
+        }
+      }
+    } match {
+      case Success(_) => Utils.log("Request handled successfully")
+      case Failure(ex) => Utils.logError("Error handling request", ex)
+    } finally {
+      clientSocket.close()
+    }
+  }
+}
