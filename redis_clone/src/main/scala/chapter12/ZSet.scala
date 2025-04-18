77a208d593a073a50094fb2fd6808d20373f26cf
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-04-17 21:38:29 -0300

    added zset definition with insert operation

--- redis_clone/src/main/scala/chapter12/ZSet.scala
+++ redis_clone/src/main/scala/chapter12/ZSet.scala
@@ -5,4 +5,24 @@ import scala.collection.mutable
 // ZNode class
 case class ZNode(name: String, var score: Double, var left: Option[ZNode] = None, var right: Option[ZNode] = None, var parent: Option[ZNode] = None)
 
+// ZSet class
+class ZSet {
+  private val hmap = mutable.HashMap[String, ZNode]()
+  private var root: Option[ZNode] = None
 
+  // Insert or update a node
+  def insert(name: String, score: Double): Boolean = {
+    hmap.get(name) match {
+      case Some(node) =>
+        update(node, score)
+        false
+      case None =>
+        val newNode = ZNode(name, score)
+        hmap.put(name, newNode)
+        treeInsert(newNode)
+        true
+    }
+  }
+
+  
+}
