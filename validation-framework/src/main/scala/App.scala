3e6c21907d6fa97f96c781d8ab896678298aebb1
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-04-22 18:09:28 -0300

    added main class to test annotation validator

---
+++ validation-framework/src/main/scala/App.scala
@@ -0,0 +1,11 @@
+@main def runValidation(): Unit = {
+  val user = User(email = "invalid-email", name = "John Doe")
+  val errors = ValidatorFramework.validate(user)
+
+  if (errors.nonEmpty) {
+    println("Validation errors:")
+    errors.foreach(println)
+  } else {
+    println("Validation passed!")
+  }
+}
