822402d9f3a216781853c6baa16aa9772da17815
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-04-23 21:50:51 -0300

    added validator for non empty annotation

---
+++ validation-framework/src/main/scala/NonEmptyValidator.scala
@@ -0,0 +1,5 @@
+object NonEmptyValidator extends Validator[String] {
+  override def validate(value: String): Boolean = value.nonEmpty
+
+  override def errorMessage: String = "Field cannot be empty"
+}
