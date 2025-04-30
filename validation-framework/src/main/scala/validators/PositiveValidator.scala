82725a7ce52d45183c95eb063d2edf9c251b586b
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-04-29 22:27:05 -0300

    moved to package for validators

---
+++ validation-framework/src/main/scala/validators/PositiveValidator.scala
@@ -0,0 +1,7 @@
+package validators
+
+object PositiveValidator extends Validator[Int] {
+  override def validate(value: Int): Boolean = value > 0
+
+  override def errorMessage: String = "Value must be positive"
+}
