9369ec1eb1138ebb20d942e8ee8bb499621a17ea
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-04-30 21:15:57 -0300

    added stress test class with method to generate random email

---
+++ validation-framework/src/test/scala/StressTest.scala
@@ -0,0 +1,14 @@
+import org.scalatest.funsuite.AnyFunSuite
+import validators.ValidatorFramework
+import models.User
+import scala.util.Random
+
+class StressTest extends AnyFunSuite {
+
+  def randomEmail(): String = {
+    val domains = List("example.com", "test.org", "sample.net")
+    s"user${Random.nextInt(10000)}@${domains(Random.nextInt(domains.length))}"
+  }
+
+
+}
