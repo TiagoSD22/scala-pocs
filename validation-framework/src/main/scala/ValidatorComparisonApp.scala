180ae98fecfa22d2ca4468e65e1a91ab1a7d5a9f
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-05-06 18:16:58 -0300

    added method to run cached and non cached validator framework and log total time

--- validation-framework/src/main/scala/ValidatorComparisonApp.scala
+++ validation-framework/src/main/scala/ValidatorComparisonApp.scala
@@ -28,5 +28,24 @@ object ValidatorComparisonApp {
     TestEntity(randomEmail(), randomName(), randomAge())
   }
 
-  
+  def main(args: Array[String]): Unit = {
+    val entityCount = 100000
+    val entities = (1 to entityCount).map(_ => randomEntity())
+
+    // Measure performance of ValidatorFramework
+    val startValidatorFramework = System.nanoTime()
+    val validatorFrameworkErrors = entities.flatMap(entity => ValidatorFramework.validate(entity))
+    val endValidatorFramework = System.nanoTime()
+
+    // Measure performance of CachedValidatorFramework
+    val startCachedValidatorFramework = System.nanoTime()
+    val cachedValidatorFrameworkErrors = entities.flatMap(entity => CachedValidatorFramework.validate(entity))
+    val endCachedValidatorFramework = System.nanoTime()
+
+    println(s"ValidatorFramework validated $entityCount entities in ${(endValidatorFramework - startValidatorFramework) / 1e9} seconds")
+    println(s"CachedValidatorFramework validated $entityCount entities in ${(endCachedValidatorFramework - startCachedValidatorFramework) / 1e9} seconds")
+
+    println(s"ValidatorFramework errors: ${validatorFrameworkErrors.size}")
+    println(s"CachedValidatorFramework errors: ${cachedValidatorFrameworkErrors.size}")
+  }
 }
