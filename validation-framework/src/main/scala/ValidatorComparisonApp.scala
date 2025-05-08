acfa510dc4434875796fcf6ac512aa0398a0d460
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-05-07 22:07:38 -0300

    added method to test performance between cached and non cached validators still not showing better results with cached version tough

--- validation-framework/src/main/scala/ValidatorComparisonApp.scala
+++ validation-framework/src/main/scala/ValidatorComparisonApp.scala
@@ -17,5 +17,24 @@ object ValidatorComparisonApp {
     Product(randomPrice(), randomName())
   }
 
+  def main(args: Array[String]): Unit = {
+    val entityCount = 100000
+    val products = (1 to entityCount).map(_ => randomProduct())
 
+    // Measure performance of ValidatorFramework
+    val startValidatorFramework = System.nanoTime()
+    val validatorFrameworkErrors = products.flatMap(product => ValidatorFramework.validate(product))
+    val endValidatorFramework = System.nanoTime()
+
+    // Measure performance of CachedValidatorFramework
+    val startCachedValidatorFramework = System.nanoTime()
+    val cachedValidatorFrameworkErrors = products.flatMap(product => CachedValidatorFramework.validate(product))
+    val endCachedValidatorFramework = System.nanoTime()
+
+    println(s"ValidatorFramework validated $entityCount products in ${(endValidatorFramework - startValidatorFramework) / 1e9} seconds")
+    println(s"CachedValidatorFramework validated $entityCount products in ${(endCachedValidatorFramework - startCachedValidatorFramework) / 1e9} seconds")
+
+    println(s"ValidatorFramework errors: ${validatorFrameworkErrors.size}")
+    println(s"CachedValidatorFramework errors: ${cachedValidatorFrameworkErrors.size}")
+  }
 }
