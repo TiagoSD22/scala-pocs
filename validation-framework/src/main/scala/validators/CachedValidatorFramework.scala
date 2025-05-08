ae4fd1126f8f07e7d2fb314a2756fa195553b440
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-05-07 22:05:50 -0300

    not using the value of the field when storing in cache

--- validation-framework/src/main/scala/validators/CachedValidatorFramework.scala
+++ validation-framework/src/main/scala/validators/CachedValidatorFramework.scala
@@ -9,35 +9,38 @@ import scala.compiletime.{constValueTuple, erasedValue, summonInline}
 import scala.deriving.Mirror
 
 object CachedValidatorFramework {
-  private val cache = mutable.Map[String, List[(String, Any, Option[StaticAnnotation])]]()
+  // Modified to store only field name and annotation
+  private val cache = mutable.Map[String, List[(String, Option[StaticAnnotation])]]()
 
   inline def validate[T](entity: T): List[String] = {
     val entityType = entity.getClass.getName
-    val fields = cache.getOrElseUpdate(entityType, extractFields(entity))
-    fields.flatMap {
-      case (name, value, Some(Email())) =>
+    val fieldMetadata = cache.getOrElseUpdate(entityType, extractFieldMetadata(entity))
+
+    // Get current field values
+    val fieldValues = entity.asInstanceOf[Product].productIterator.toList
+
+    // Combine metadata with current values
+    fieldMetadata.zip(fieldValues).flatMap {
+      case ((name, Some(Email())), value) =>
         if (!EmailValidator.validate(value.toString)) Some(s"Field '$name': ${EmailValidator.errorMessage}")
         else None
-      case (name, value, Some(NonEmpty())) =>
+      case ((name, Some(NonEmpty())), value) =>
         if (!NonEmptyValidator.validate(value.toString)) Some(s"Field '$name': ${NonEmptyValidator.errorMessage}")
         else None
-      case (name, value, Some(Positive())) =>
+      case ((name, Some(Positive())), value) =>
         if (!PositiveValidator.validate(value.asInstanceOf[Int])) Some(s"Field '$name': ${PositiveValidator.errorMessage}")
         else None
       case _ => None
     }
   }
 
-  private inline def extractFields[T](entity: T): List[(String, Any, Option[StaticAnnotation])] = {
+  private inline def extractFieldMetadata[T](entity: T): List[(String, Option[StaticAnnotation])] = {
     inline erasedValue[T] match {
       case _: Mirror.ProductOf[T] =>
         val mirror = summonInline[Mirror.ProductOf[T]]
         val fieldNames = constValueTuple[mirror.MirroredElemLabels].toList.asInstanceOf[List[String]]
-        val fieldValues = entity.asInstanceOf[Product].productIterator.toList
         val fieldAnnotations = summonFieldAnnotations[mirror.MirroredElemTypes]
-        fieldNames.zip(fieldValues).zip(fieldAnnotations).map {
-          case ((name, value), annotation) => (name, value, annotation)
-        }
+        fieldNames.zip(fieldAnnotations)
       case _ => List.empty
     }
   }
