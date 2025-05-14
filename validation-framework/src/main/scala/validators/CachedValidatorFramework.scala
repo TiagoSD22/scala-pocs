1b188bb2114b7e5f4154109fec089cba3e83310f
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-05-13 22:33:38 -0300

    tried an approach to avoid inline call of mirror during validation

--- validation-framework/src/main/scala/validators/CachedValidatorFramework.scala
+++ validation-framework/src/main/scala/validators/CachedValidatorFramework.scala
@@ -8,52 +8,47 @@ import scala.compiletime.{constValueTuple, erasedValue, summonInline}
 import scala.deriving.Mirror
 
 object CachedValidatorFramework {
-  // Cache stores field name and annotation pairs
+  // Cache for field metadata
   private val cache = mutable.Map[Class[_], List[(String, Option[StaticAnnotation])]]()
 
   inline def validate[T](entity: T): List[String] = {
-    // Use class object directly as key (faster than getName)
     val entityClass = entity.getClass
-    val fieldMetadata = cache.getOrElseUpdate(entityClass, extractFieldMetadata(entity))
 
-    // Get values directly without intermediate List creation
+    // Get metadata from cache or compute it once
+    val fieldMetadata = cache.getOrElseUpdate(entityClass, {
+      inline erasedValue[T] match {
+        case _: Mirror.ProductOf[T] =>
+          val mirror = summonInline[Mirror.ProductOf[T]]
+          val fieldNames = constValueTuple[mirror.MirroredElemLabels].toList.asInstanceOf[List[String]]
+          val fieldAnnotations = summonFieldAnnotations[mirror.MirroredElemTypes]
+          fieldNames.zip(fieldAnnotations)
+        case _ => List.empty
+      }
+    })
+
+    // Fast validation using cached metadata
     val productEntity = entity.asInstanceOf[scala.Product]
-
-    // Avoid creating intermediate collections with direct iteration
     var errors = List.empty[String]
     var i = 0
-    while (i < fieldMetadata.length) {
+    val len = fieldMetadata.length
+
+    while (i < len) {
       val (name, annotOpt) = fieldMetadata(i)
       val value = productEntity.productElement(i)
 
       annotOpt match {
-        case Some(Email()) =>
-          if (!EmailValidator.validate(value.toString))
-            errors = s"Field '$name': ${EmailValidator.errorMessage}" :: errors
-        case Some(NonEmpty()) =>
-          if (!NonEmptyValidator.validate(value.toString))
-            errors = s"Field '$name': ${NonEmptyValidator.errorMessage}" :: errors
-        case Some(Positive()) =>
-          if (!PositiveValidator.validate(value.asInstanceOf[Int]))
-            errors = s"Field '$name': ${PositiveValidator.errorMessage}" :: errors
+        case Some(Email()) if !EmailValidator.validate(value.toString) =>
+          errors = s"Field '$name': ${EmailValidator.errorMessage}" :: errors
+        case Some(NonEmpty()) if !NonEmptyValidator.validate(value.toString) =>
+          errors = s"Field '$name': ${NonEmptyValidator.errorMessage}" :: errors
+        case Some(Positive()) if !PositiveValidator.validate(value.asInstanceOf[Int]) =>
+          errors = s"Field '$name': ${PositiveValidator.errorMessage}" :: errors
         case _ => // No validation needed
       }
       i += 1
     }
 
-    errors.reverse
-  }
-
-  // Keep the metadata extraction methods as they were
-  private inline def extractFieldMetadata[T](entity: T): List[(String, Option[StaticAnnotation])] = {
-    inline erasedValue[T] match {
-      case _: Mirror.ProductOf[T] =>
-        val mirror = summonInline[Mirror.ProductOf[T]]
-        val fieldNames = constValueTuple[mirror.MirroredElemLabels].toList.asInstanceOf[List[String]]
-        val fieldAnnotations = summonFieldAnnotations[mirror.MirroredElemTypes]
-        fieldNames.zip(fieldAnnotations)
-      case _ => List.empty
-    }
+    if (errors.isEmpty) List.empty else errors.reverse
   }
 
   private inline def summonFieldAnnotations[T <: Tuple]: List[Option[StaticAnnotation]] = inline erasedValue[T] match {
